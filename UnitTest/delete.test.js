da738d75c7af5c9a11f2e1bbff634511ea2f32d3
PirateKingDee <andyliang987@hotmail.com>
2017-10-31 21:08:16 -0700

    change config

--- UnitTest/delete.test.js
+++
@@ -1,8 +0,0 @@
-import {assert} from 'chai';
-import { chai } from 'meteor/practicalmeteor:chai';
-import '/imports/api/jobs/server/methods/delete_job.js';
-describe('testing out delete', function () {
-  it('assert delete', function () {
-    assert.equal(Meteor.call("jobRemove",{id:"testing"}), 'success');
-  });
-});
